ef0b5ddc8774202794c97d8d9e123c87ee770413
Rafeek Asharaf <rafeek717@gmail.com>
2026-03-16 21:57:25 +0530

    [Seo optimizarion and address change]

--- key-recycling-website/src/components/Footer.tsx
+++ key-recycling-website/src/components/Footer.tsx
@@ -50,7 +50,7 @@ export default function Footer() {
                     <a href="tel:+14843691217">+1 (484) 369-1217</a>
 
                     <div className="muted" style={{ marginTop: 10 }}>Address</div>
-                    <div>8400 US-13, Bristol, PA 19007</div>
+                    <div>1800 Route-13, Bristol, PA 19007</div>
                 </div>
             </div>
 
